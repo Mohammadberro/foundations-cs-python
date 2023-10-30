5d9d8f179ae2a0ab675c8e650a7cf9046442b9b0
Mohammadberro <ikerberro1@gmail.com>
2023-10-30 19:47:44 +0200

    ass1

---
+++ ass1.py
@@ -0,0 +1,8 @@
+top = float(input("Enter the top number"))
+bottom = float(input("Enter the top bottom number"))
+val = float(input("Enter the val"))
+
+if top > val > bottom:
+    print("true")
+else:
+    print("false")
